ed95a49649e46fa02006482484faa93d2d29d869
Noc <t.gelotte@gmail.com>
2025-03-17 19:30:27 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+
+void setup() {
+
+  Serial.begin(115200);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+
+  
+
+}
+
